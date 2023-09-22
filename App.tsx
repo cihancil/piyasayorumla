430e73b33406eaf7592dd74acf48544c46b51cac
Cihan Cil <cihancil@gmail.com>
2023-09-22 13:45:18 +0300

    navigator

--- App.tsx
+++ App.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import {
-  SafeAreaView
+  View,
+  Text,
 } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
-
   return (
-    <SafeAreaView>
-    </SafeAreaView>
+    <NavigationContainer>
+      {/* <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
